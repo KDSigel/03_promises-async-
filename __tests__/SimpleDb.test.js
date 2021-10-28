625ada6e2974b4c80969d34ec17bf7f0271014fa
Karl Sigel <heykarl@gmail.com>
2021-10-28 10:39:29 -0700

    created test to create and save file, and to check that it's where I put it

--- __tests__/SimpleDb.test.js
+++ __tests__/SimpleDb.test.js
@@ -1,1 +1,27 @@
+const { mkdir, rm } = require('fs/promises');
 const SimpleDb = require('../lib/SimpleDb.js');
+
+describe('SimpleDb', () => {
+  const rootDir = './__tests__/store';
+
+  beforeEach(() => {
+    return rm(rootDir, { force: true, recursive: true }).then(() =>
+      mkdir(rootDir, { recursive: true })
+    );
+  });
+
+  it('should create a file from an object and save somewhere', () => {
+
+    const someNewObject = { hu_mon: 'karl' };
+    const finalFile = { 
+      hu_mon: 'karl',
+      id: expect.any(String)
+    };
+
+    const simpleDb = new SimpleDb(rootDir);
+    return simpleDb
+      .save(someNewObject)
+      .then(() => simpleDb.get(someNewObject.id))
+      .then((getReturns) => expect(getReturns).toEqual(finalFile));
+  });
+});
